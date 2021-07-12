0d4a56e16afa0437917b41449921899c1d8f8330
Konstantin Mark <konstantin.mark99@gmail.com>
2021-07-12 17:38:10 +0200

    Add files via upload

---
+++ Benchmarks/increase_n_ints.py
@@ -0,0 +1,72 @@
+import pyomo.environ as pyo
+import pyomo
+from Stkw_Linear.plin_models import *
+from Stkw_Linear.plin_benchmarking import *
+import numpy as np
+import pandas
+
+
+settings = [
+        {
+        "n_its": 10,
+        "n_intervals": 16,
+        "rng": np.random.default_rng(2021),
+        "time_limit": 10
+        },{
+        "n_its": 10,
+        "n_intervals": 32,
+        "rng": np.random.default_rng(2021),
+        "time_limit": 10
+        },
+        {
+        "n_its": 10,
+        "n_intervals": 64,
+        "rng": np.random.default_rng(2021),
+        "time_limit": 10
+        },
+        {
+        "n_its": 10,
+        "n_intervals": 128,
+        "rng": np.random.default_rng(2021),
+        "time_limit": 10
+        },
+        {
+        "n_its": 10,
+        "n_intervals": 256,
+        "rng": np.random.default_rng(2021),
+        "time_limit": 10
+        }
+    ]
+
+#Concave Benchmark
+B_c = ConcaveBenchmark(**settings[0])
+#add solvers
+gurobi = B_Solver("gurobi")
+cplex = B_Solver("cplex")
+#add models
+
+
+m_models = [
+    Indicator_Strong_Concrete(),
+    Incremental_Concrete(),
+    DLog_Concrete(),
+    ]
+
+
+B_c.add_solver(gurobi)
+B_c.add_solver(cplex)
+
+for model in m_models: 
+    B_c.add_model(model)
+
+Benchmarks = [
+    B_c
+    ]
+
+for setting in settings:
+    for Benchmark in Benchmarks:
+        print("Starting Benchmark")
+        Benchmark.set_benchmark_parameters(**setting)
+        Benchmark.run(5)
+        Benchmark.save_output("data\\increase_N_its\\")
+        Benchmark.reset_output()
